94a79ab936d8703acff09af3bfad78234954f274
sinhadivyanshi22 <sinhadivyannshi2205@gmail.com>
2026-08-03 00:51:28 +0530

    Pattern 3

---
+++ pattern3.java
@@ -0,0 +1,24 @@
+//1
+//12
+//123
+///1234
+///12345
+
+import java.util.Scanner;
+public class pattern3 {
+    void solution(int n){
+        for (int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        pattern3 obj = new pattern3();
+        obj.solution(n);
+        sc.close();
+    }
+}
