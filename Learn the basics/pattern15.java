1e59a70702279ab21a0578da106ce045344712c4
sinhadivyanshi22 <sinhadivyannshi2205@gmail.com>
2026-08-10 03:34:22 +0530

    15 - Reverse Letter Triangle

---
+++ Learn the basics/pattern15.java
@@ -0,0 +1,27 @@
+// A B C D E 
+// A B C D 
+// A B C 
+// A B 
+// A 
+
+import java.util.Scanner;
+public class pattern15 {
+    
+    public void solve(int n){
+        for(int i=1; i<=n; i++){
+            for(char ch='A'; ch<='A'+(n-i); ch++){//A+ (BCDE)N-1
+                System.out.print(ch+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        pattern15 obj = new pattern15();
+        obj.solve(n);
+        sc.close();
+    }
+    
+}
