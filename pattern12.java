fb5390d464876fd59e90a67525322edd64465916
sinhadivyanshi22 <sinhadivyannshi2205@gmail.com>
2026-08-10 02:09:30 +0530

    Pattern 12 - Number Crown Pattern

--- pattern12.java
+++ pattern12.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
 public class pattern12 {
-    
+    public void solve(int n){
+        int space= 2*(n-1);
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){//number
+                System.out.print(j);
+            }
+            for(int j=1; j<=space; j++){//space
+                System.out.print(" ");
+            }
+            for(int j=i; j>=1; j--){//number
+                System.out.print(j);
+            }
+            space=space-2;
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pattern12 obj = new pattern12();
+        obj.solve(n);
+        sc.close();
+    }
 }
