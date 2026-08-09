b0136b17be5d79883d7f3f5f926f96f0633fa79c
sinhadivyanshi22 <sinhadivyannshi2205@gmail.com>
2026-08-10 03:42:33 +0530

    16 - Alpha Ramp

---
+++ Learn the basics/pattern16.java
@@ -0,0 +1,27 @@
+// A 
+// B B 
+// C C C 
+// D D D D 
+// E E E E E 
+
+import java.util.Scanner;
+public class pattern16 {
+    
+    public void solve(int n){
+        for(int i=1; i<=n; i++){
+            char ch= (char)('A' + i-1);
+            for(int j=1; j<=i; j++){
+                System.out.print(ch+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        pattern16 obj = new pattern16();
+        obj.solve(n);
+        sc.close();
+    }
+}
