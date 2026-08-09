bd418d8baabdbd9a694c84aa1e848128e6439ae3
sinhadivyanshi22 <sinhadivyannshi2205@gmail.com>
2026-08-10 02:54:12 +0530

    Arranged the codes in Folder

--- Learn the basics/Build up logic/pattern10.java
+++ Learn the basics/Build up logic/pattern10.java
@@ -1,3 +1,4 @@
+//SOLVE 1
 // *---
 // **--
 // ***-
@@ -6,9 +7,20 @@
 // **--
 // *---
 
+//SOLVE 2
+// *
+// **
+// ***
+// ****
+// *****
+// ****
+// ***
+// **
+// *
 import java.util.Scanner;
 public class pattern10 {
-    public void solve(int n) {
+
+    public void solve1(int n) {
         for(int i=1; i<=n; i++){
             for(int j=1; j<=i; j++){
                 System.out.print("*");
@@ -29,11 +41,23 @@ public class pattern10 {
         }
     }
 
+    public void solve2(int n){
+        for(int i=1; i<=2*n-1; i++){
+            int star=i;
+            if(i>n) star=2*n-i;
+            for(int j=1; j<=star; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         pattern10 obj=new pattern10();
-        obj.solve(n);
+        obj.solve1(n);
+        obj.solve2(n);
         sc.close();
     }
 }
