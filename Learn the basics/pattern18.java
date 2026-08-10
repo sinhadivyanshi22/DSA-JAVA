7ebd4df00be04496aa45178b192ac0783d95a79a
sinhadivyanshi22 <sinhadivyannshi2205@gmail.com>
2026-08-10 21:48:29 +0530

    18 - Alpha Triangle Pattern

--- Learn the basics/pattern18.java
+++ Learn the basics/pattern18.java
@@ -2,5 +2,20 @@
 
 import java.util.Scanner;
 public class pattern18 {
-    
+
+    public void solve(int n){
+        for(int i=1; i<=n; i++){
+            for(char ch=(char)('E'-i+1); ch<='E'; ch++){
+                System.out.print(ch+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        pattern18 obj = new pattern18();
+        obj.solve(n);
+        sc.close();
+    }
 }
