98f5b1c51485c239d8e1474543f09c1c1a6580f4
sinhadivyanshi22 <sinhadivyannshi2205@gmail.com>
2026-08-17 21:20:32 +0530

    16-Alpha Ramp Triangle

--- Learn the basics/pattern16.java
+++ Learn the basics/pattern16.java
@@ -16,7 +16,6 @@ public class pattern16 {
             System.out.println();
         }
     }
-
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
