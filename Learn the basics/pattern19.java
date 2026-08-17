6446bbeca38a7c524748b5a637a7224323eb87d2
sinhadivyanshi22 <sinhadivyannshi2205@gmail.com>
2026-08-18 02:15:06 +0530

    19-Symmetric Void

--- Learn the basics/pattern19.java
+++ Learn the basics/pattern19.java
@@ -1,14 +1,47 @@
+// ******
+// **  **
+// *    *
+// *    *
+// **  **
+// ******
 import java.util.Scanner;
 public class pattern19 {
 
     public void solve(int n){
+        int ins=0;//taken for spaces
+
+        //upperhalf
         for(int i=0; i<n; i++){
-            for(char ch=(char)('E'-i); ch<='E'; ch++){
-                System.out.print(ch+" ");
+            for(int j=0; j<=n-i-1; j++){//stars
+                System.out.print("*");
             }
+            for(int j=0; j<ins; j++){//space
+                System.out.print(" ");
+            }
+            for(int j=0; j<=n-i-1; j++){//stars
+                System.out.print("*");
+            }
+            ins+=2;
+            System.out.println();
+        }
+        
+        //lowerhalf
+        ins=2*n-2;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){//stars
+                System.out.print("*");
+            }
+            for(int j=0; j<ins; j++){//space
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){//stars
+                System.out.print("*");
+            }
+            ins-=2;
             System.out.println();
         }
     }
+
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
