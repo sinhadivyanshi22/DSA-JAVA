112fc5f9269a503b62f980ffde8abb7561a6c662
sinhadivyanshi22 <sinhadivyannshi2205@gmail.com>
2026-08-17 21:19:43 +0530

    15-Reverse Letter Triangle

--- Learn the basics/pattern15.java
+++ Learn the basics/pattern15.java
@@ -15,7 +15,6 @@ public class pattern15 {
             System.out.println();
         }
     }
-
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
