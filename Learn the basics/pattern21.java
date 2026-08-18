4dd403225e05402c1d0d2427f787a7a7cd03fcad
sinhadivyanshi22 <sinhadivyannshi2205@gmail.com>
2026-08-19 02:17:48 +0530

    21-Hollow Rectangle

---
+++ Learn the basics/pattern21.java
@@ -0,0 +1,25 @@
+// *****
+// *   *
+// *   *
+// *   *
+// *****
+import java.util.Scanner;
+public class pattern21{
+    public void solve(int n){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 || i==n-1 || j==0 || j==n-1) System.out.print("*");
+                else System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pattern21 obj = new pattern21();
+        obj.solve(n);
+        sc.close();
+    }
+    
+}
---
+++ Learn the basics/pattern22.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class pattern22{
+    public void solve(int n){
+        
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pattern22 obj = new pattern22();
+        obj.solve(n);
+        sc.close();
+    }
+}
