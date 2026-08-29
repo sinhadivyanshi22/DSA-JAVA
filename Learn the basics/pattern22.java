8d0e665b8a32201c933f43911d86d89554dfbdc2
sinhadivyanshi22 <sinhadivyannshi2205@gmail.com>
2026-08-30 02:57:18 +0530

    22-Number Pattern

--- Learn the basics/pattern22.java
+++ Learn the basics/pattern22.java
@@ -1,9 +1,18 @@
-444444444
 
 import java.util.Scanner;
 public class pattern22{
     public void solve(int n){
-
+        for(int i=0; i<2*n-1; i++){
+            for(int j=0; j<2*n-1; j++){
+                int top=i;
+                int bottom=(2*n-2)-i;
+                int left=j;
+                int right=(2*n-2)-j;
+                int mini=Math.min(Math.min(top,bottom),Math.min(left,right));
+                System.out.print(n-mini);
+            }
+            System.out.println();
+        }
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
