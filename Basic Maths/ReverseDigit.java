c99f5417781f145581a91e042ddabbf28c78175d
sinhadivyanshi22 <sinhadivyannshi2205@gmail.com>
2026-09-02 01:35:21 +0530

    Reverse Digits

--- Basic Maths/ReverseDigit.java
+++ Basic Maths/ReverseDigit.java
@@ -1,1 +1,24 @@
-
+import java.util.Scanner;
+public class ReverseDigit{
+    public int reverse(int num){
+        int reversed = 0;
+        while(num != 0){
+            int digit = num % 10;
+            if(reversed >Integer.MAX_VALUE/10 || reversed<Integer.MIN_VALUE/10){
+                return 0;
+            }
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+        return reversed;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        ReverseDigit obj = new ReverseDigit();
+        int reversed=obj.reverse(num);
+        System.out.println("Reversed Number: " + reversed);
+        sc.close();
+    }
+}
