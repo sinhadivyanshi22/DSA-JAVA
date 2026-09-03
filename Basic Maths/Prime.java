c10975a4080b31a7cc9757d565aed616d31e4450
sinhadivyanshi22 <sinhadivyannshi2205@gmail.com>
2026-09-04 05:10:05 +0530

    Prime

--- Basic Maths/Prime.java
+++ Basic Maths/Prime.java
@@ -1,1 +1,30 @@
-
+import java.util.Scanner;
+public class Prime{
+    public void isprime(int n){
+        //for negative no
+        if(n<=1){
+            System.out.println("Not Prime");
+            return;
+        }
+        
+        int c=0;
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0){
+                c++;
+            }
+        }
+        if(c==0){
+            System.out.println("Prime");
+        }
+        else{
+            System.out.println("Not Prime");
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        Prime obj=new Prime();
+        obj.isprime(n);
+        sc.close();
+    }
+}
