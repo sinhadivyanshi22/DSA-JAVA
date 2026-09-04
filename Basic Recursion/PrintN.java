0e67fcd4625c828e030704dc1e88f492668bb2cd
sinhadivyanshi22 <sinhadivyannshi2205@gmail.com>
2026-09-04 19:09:42 +0530

    Print something N

--- Basic Recursion/PrintN.java
+++ Basic Recursion/PrintN.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 class PrintN{
     int cnt=0;
     public void print(){
@@ -10,7 +10,9 @@ class PrintN{
         print();
     }
     public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
         PrintN obj=new PrintN();
         obj.print();
+        sc.close();
     }
 }
