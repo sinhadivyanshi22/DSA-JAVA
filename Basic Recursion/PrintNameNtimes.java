cfd81b014e68d6c90324db2c69cf73d56a10427a
sinhadivyanshi22 <sinhadivyannshi2205@gmail.com>
2026-09-04 21:42:21 +0530

    Print Name n times

--- Basic Recursion/PrintNameNtimes.java
+++ Basic Recursion/PrintNameNtimes.java
@@ -1,1 +1,18 @@
-
+import java.util.Scanner;
+public class PrintNameNtimes{
+    public static void printName(String name, int i,int n){
+        if(i>n) return;
+        else{
+            System.out.println(i+" "+name);
+            i++;
+            printName(name,i,n);
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String name=sc.next();
+        int n=sc.nextInt();
+        printName(name,1,n);
+        sc.close();
+    }
+}
